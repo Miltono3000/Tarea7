a32f39844731bb9a752db13542483cc163090afd
Miltono3000 <159266951+Miltono3000@users.noreply.github.com>
2024-04-07 23:15:40 -0600

    Update UniryOrdenar.js

--- UniryOrdenar.js
+++ UniryOrdenar.js
@@ -1,7 +1,36 @@
-module.exports = function UniryOrdenar(array1, array2){
-    let array3 = array1.concat(array2);
-    array3.sort((a, b) => a-b);
+module.exports = function UniryOrdenar(A, B) {
+    let C = [];
+    let i = 0, j = 0, k = 0;
+    while (i < A.length && j < B.length) {
+        if (A[i] < B[j]) {
+            C[k] = A[i];
+            i++;
+        } else {
+            C[k] = B[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < A.length) {
+        C[k] = A[i];
+        i++;
+        k++;
+    }
+    while (j < B.length) {
+        C[k] = B[j];
+        j++;
+        k++;
+    }
 
-    return array3;
+    for (let i = 0; i < C.length - 1; i++) {
+        for (let j = 0; j < C.length - i - 1; j++) {
+            if (C[j] > C[j + 1]) {
+                let temp = C[j];
+                C[j] = C[j + 1];
+                C[j + 1] = temp;
+            }
+        }
+    }
+
+    return C;
 }
-
